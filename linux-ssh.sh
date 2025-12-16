c53a72c93173e7b4b374dee0a440a1f771766d51
emmy554419 <121647923+emmy554419@users.noreply.github.com>
2025-12-16 14:12:02 +0100

    Update linux-ssh.sh

--- linux-ssh.sh
+++ linux-ssh.sh
@@ -1,12 +1,15 @@
-#linux-run.sh LINUX_USER_PASSWORD NGROK_AUTH_TOKEN LINUX_USERNAME LINUX_MACHINE_NAME
 #!/bin/bash
-# /home/runner/.ngrok2/ngrok.yml
+# Required secrets:
+# NGROK_AUTH_TOKEN
+# LINUX_USER_PASSWORD
+# Optional:
+# LINUX_MACHINE_NAME
 
-sudo useradd -m $LINUX_USERNAME
-sudo adduser $LINUX_USERNAME sudo
-echo "$LINUX_USERNAME:$LINUX_USER_PASSWORD" | sudo chpasswd
-sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
-sudo hostname $LINUX_MACHINE_NAME
+set -e
+
+USER="runner"
+
+echo "### Checking required secrets ###"
 
 if [[ -z "$NGROK_AUTH_TOKEN" ]]; then
   echo "Please set 'NGROK_AUTH_TOKEN'"
@@ -14,36 +17,36 @@ if [[ -z "$NGROK_AUTH_TOKEN" ]]; then
 fi
 
 if [[ -z "$LINUX_USER_PASSWORD" ]]; then
-  echo "Please set 'LINUX_USER_PASSWORD' for user: $USER"
+  echo "Please set 'LINUX_USER_PASSWORD'"
   exit 3
 fi
 
-echo "### Install ngrok ###"
+echo "### Set hostname (optional) ###"
+if [[ -n "$LINUX_MACHINE_NAME" ]]; then
+  sudo hostname "$LINUX_MACHINE_NAME"
+fi
 
-wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
-unzip ngrok-stable-linux-386.zip
-chmod +x ./ngrok
-
-echo "### Update user: $USER password ###"
+echo "### Set SSH password for runner ###"
 echo -e "$LINUX_USER_PASSWORD\n$LINUX_USER_PASSWORD" | sudo passwd "$USER"
 
-echo "### Start ngrok proxy for 22 port ###"
+echo "### Install ngrok v3 ###"
+wget -q https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.zip
+unzip -o ngrok-v3-stable-linux-amd64.zip
+chmod +x ngrok
+sudo mv ngrok /usr/local/bin/ngrok
 
+echo "### Configure ngrok ###"
+ngrok config add-authtoken "$NGROK_AUTH_TOKEN"
 
-rm -f .ngrok.log
-./ngrok authtoken "$NGROK_AUTH_TOKEN"
-./ngrok tcp 22 --log ".ngrok.log" &
+echo "### Start ngrok TCP tunnel for SSH (port 22) ###"
+rm -f ngrok.log
+ngrok tcp 22 --log=stdout > ngrok.log &
 
-sleep 10
-HAS_ERRORS=$(grep "command failed" < .ngrok.log)
+sleep 8
 
-if [[ -z "$HAS_ERRORS" ]]; then
-  echo ""
-  echo "=========================================="
-  echo "To connect: $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh $USER@/" | sed "s/:/ -p /")"
-  echo "or conenct with $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh (Your Linux Username)@/" | sed "s/:/ -p /")"
-  echo "=========================================="
-else
-  echo "$HAS_ERRORS"
-  exit 4
-fi
+echo ""
+echo "=========================================="
+echo "SSH CONNECTION COMMAND:"
+grep -oE "tcp://[a-z0-9\.]+:[0-9]+" ngrok.log | \
+sed "s#tcp://#ssh runner@#; s#:# -p #"
+echo "=========================================="
